7da4f42dd12abd31bcf0cf81ea68f821fb1c6fc9
Jaswanth <vishnugajulapalli123@gmail.com>
2020-10-12 08:36:28 +0530

    visibility changes

--- 01/app.js
+++ 01/app.js
@@ -1,4 +1,7 @@
+document.getElementById('output').style.visibility = 'hidden'
+
 document.getElementById('lbsInput').addEventListener('input', function(e){
+    document.getElementById('output').style.visibility = 'visible'
     let lbs = e.target.value;
     document.getElementById('gramsOutput').innerHTML = (lbs/0.0022046).toFixed(2)
     document.getElementById('kilo_gramsOutput').innerHTML = (lbs/2.2046).toFixed(2)
