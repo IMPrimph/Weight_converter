e6d3b824ebdd3c21781893e45ba1966916cce6cd
Jaswanth <vishnugajulapalli123@gmail.com>
2020-10-12 08:03:11 +0530

    initial commit

---
+++ 01/app.js
@@ -0,0 +1,6 @@
+document.getElementById('lbsInput').addEventListener('input', function(e){
+    let lbs = e.target.value;
+    document.getElementById('gramsOutput').innerHTML = (lbs/0.0022046).toFixed(2)
+    document.getElementById('kilo_gramsOutput').innerHTML = (lbs/2.2046).toFixed(2)
+    document.getElementById('ouncesOutput').innerHTML = (lbs * 16).toFixed(2)
+})
